a5d9c313ff44f04d60e9b7a4520882f409f2b539
Didvalerij <didvalerij@yandex.ru>
2016-03-06 23:24:21 +0200

    Added files via upload

---
+++ Modul1.java
@@ -0,0 +1,8 @@
+/**
+ * Created by user on 06.03.2016.
+ */
+public class Modul1 {
+    public static void main(String[] args){
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
+    }
+}
